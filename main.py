75785af3a0225ef82bbfaa30add5db894b160357
daniel-ubuntu <elhadan@post.bgu.ac.il>
2022-01-14 13:09:40 +0200

    WIP

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+from DTO import Hats, Suppliers
+from Repository import repo
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+
+def start(config_path, orders_path):
+    repo.__init__()
+    # repo.create_tables()
+    with open(config_path) as config:
+        firstline = config.readline()
+        param = firstline.split()[0]
+        param = param.split(",")
+        num_of_hats = (int)(param[0])
+        num_of_suppliers = (int)(param[1])
+        lines = config.readlines()
+        count = 0
+        for line in lines:
+            if count != 0 & num_of_hats != 0:
+                hat_details = firstline.split()[0]
+                hat_details = hat_details.split(",")
+                id = (int)(hat_details[0])
+                topping = hat_details[1]
+                supplier = (int)(hat_details[2])
+                quantity = (int)(hat_details[3])
+                num_of_hats = num_of_hats - 1
+            count = count + 1
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        while(range(num_of_hats - 1) != num_of_hats):
+            (id, topping, supplier, quantity) = os.path.splitext(os.listdir(config_path))
+            hat = Hats.__init__(id, topping, supplier, quantity)
+            repo.hats.insert(hat)
+    # while(range(num_of_suppliers - 1) != num_of_suppliers):
+    #     (id, name) = os.path.splitext(os.listdir(config_path))
+    #     supplier = Suppliers.__init__(id, name)
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+        start("config.txt", "orders.txt")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
